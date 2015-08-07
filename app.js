b4b9a5b2d4eb30cff26dc9372fce4067943fc4ad
oyama <oyama@projectcodex.co>
2015-08-07 16:11:51 +0200

    port is changed

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var products = require('./routes/products');
 
 
 var app = express();
@@ -9,11 +11,21 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public' ));
 
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 app.get('/', function(req, res){
 	res.render('home')
 }); 
 
-var port = process.env.port || 3000;
+app.get('/products', products.show);
+app.post('/products/add', products.add);
+// app.post('/products/update', products.update);
+// app.get('/products/complaint', products.complaint);
+// app.get('/products/register', products.register);
+
+var port = process.env.port || 1999;
 
 app.listen(port, function(){
 	console.log('listening on *:' + port);
